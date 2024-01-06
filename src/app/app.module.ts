716734584ac3640c86088e7e9d519626cd58c3d5
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2024-01-06 15:00:13 +0200

    Finished photo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccordionModule } from 'primeng/accordion';
@@ -145,6 +145,7 @@ import { GalleryLightboxComponent } from './gallery-lightbox/gallery-lightbox.co
     BrowserModule,
     AppRoutingModule,
     MenubarModule,
+    BrowserAnimationsModule,
     MenuModule,
     GoogleMapsModule,
     AvatarModule,
--- src/app/gallery-lightbox/gallery-lightbox.component.ts
+++ src/app/gallery-lightbox/gallery-lightbox.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  style,
+  transition,
+  trigger,
+  AnimationEvent,
+} from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 
 interface Item {
@@ -8,18 +15,36 @@ interface Item {
 @Component({
   selector: 'app-gallery-lightbox',
   templateUrl: './gallery-lightbox.component.html',
-  styleUrls: ['./gallery-lightbox.component.scss']
+  styleUrls: ['./gallery-lightbox.component.scss'],
+  animations: [
+    trigger('animation', [
+      transition('void => visible', [
+        style({ transform: 'scale(0.5)' }),
+        animate('150ms', style({ transform: 'scale(1)' })),
+      ]),
+      transition('visible => void', [
+        style({ transform: 'scale(1)' }),
+        animate('150ms', style({ transform: 'scale(0.5)' })),
+      ]),
+    ]),
+    trigger('animation2', [
+      transition(':leave',[
+        style({opacity:1}),
+        animate('100ms', style({opacity: 0.8}))
+      ])
+    ])
+  ],
 })
 export class GalleryLightboxComponent implements OnInit {
   @Input() galleryData: Item[] = [];
-  @Input() showCount:boolean = false;
+  @Input() showCount: boolean = false;
 
-  previewImage:boolean = false;
-  showMask:boolean = false;
+  previewImage: boolean = false;
+  showMask: boolean = false;
   currentLightboxImage: Item = this.galleryData[0];
-  currentIndex:number = 0;
-  controls:boolean = true;
-  totalImageCount:number = 0;
+  currentIndex: number = 0;
+  controls: boolean = true;
+  totalImageCount: number = 0;
 
   ngOnInit(): void {
     this.totalImageCount = this.galleryData.length;
@@ -30,5 +55,29 @@ export class GalleryLightboxComponent implements OnInit {
     this.previewImage = true;
     this.currentIndex = index;
     this.currentLightboxImage = this.galleryData[index];
+  }
+
+  onAnimationEnd(event: AnimationEvent): void {
+    if (event.toState === 'void') {
+      this.showMask = false;
+    }
+  }
+
+  onClosePreview(): void {
+    this.previewImage = false;
+  }
+  prev(): void {
+    this.currentIndex = this.currentIndex - 1; 
+    if(this.currentIndex < 0 ) {
+      this.currentIndex = 0;
+    }
+    this.currentLightboxImage = this.galleryData[this.currentIndex];
+  }
+  next(): void {
+    this.currentIndex = this.currentIndex + 1; 
+    if(this.currentIndex > this.galleryData.length - 1) {
+      this.currentIndex = 0;
+    }
+    this.currentLightboxImage = this.galleryData[this.currentIndex];
   }
 }
