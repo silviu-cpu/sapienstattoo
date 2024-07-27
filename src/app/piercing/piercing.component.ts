6aa8cf3b5fe3d362768ffa0e6f26ca050efc7d36
Silviu Barbu <s.barbu@goit.global>
2024-07-27 20:57:53 +0300

    Added piercing comp & fixed some bugs

--- src/app/piercing/piercing.component.ts
+++ src/app/piercing/piercing.component.ts
@@ -1,10 +1,196 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  imageSrc: string;
+  imageAlt: string;
+}
+
 @Component({
   selector: 'app-piercing',
   templateUrl: './piercing.component.html',
   styleUrls: ['./piercing.component.scss']
 })
-export class PiercingComponent {
 
+export class PiercingComponent {
+  data: Item[] = [
+    {
+      imageSrc: "../../assets/Piercing/1.png", 
+      imageAlt: "Photo 1"
+    },
+    {
+      imageSrc: "../../assets/Piercing/2.png", 
+      imageAlt: "Photo 2"
+    },
+    {
+      imageSrc: "../../assets/Piercing/3.png", 
+      imageAlt: "Photo 3"
+    },
+    {
+      imageSrc: "../../assets/Piercing/4.png", 
+      imageAlt: "Photo 4"
+    },
+    {
+      imageSrc: "../../assets/Piercing/5.png", 
+      imageAlt: "Photo 5"
+    },
+    {
+      imageSrc: "../../assets/Piercing/6.png", 
+      imageAlt: "Photo 6"
+    },
+    {
+      imageSrc: "../../assets/Piercing/7.png", 
+      imageAlt: "Photo 7"
+    },
+    {
+      imageSrc: "../../assets/Piercing/8.png", 
+      imageAlt: "Photo 8"
+    },
+    {
+      imageSrc: "../../assets/Piercing/9.png", 
+      imageAlt: "Photo 9"
+    },
+    {
+      imageSrc: "../../assets/Piercing/10.png", 
+      imageAlt: "Photo 10"
+    },
+    {
+      imageSrc: "../../assets/Piercing/11.png", 
+      imageAlt: "Photo 11"
+    },
+    {
+      imageSrc: "../../assets/Piercing/12.png", 
+      imageAlt: "Photo 12"
+    },
+    {
+      imageSrc: "../../assets/Piercing/13.png", 
+      imageAlt: "Photo 13"
+    },
+    {
+      imageSrc: "../../assets/Piercing/14.png", 
+      imageAlt: "Photo 14"
+    },
+    {
+      imageSrc: "../../assets/Piercing/15.png", 
+      imageAlt: "Photo 15"
+    },
+    {
+      imageSrc: "../../assets/Piercing/16.png", 
+      imageAlt: "Photo 16"
+    },
+    {
+      imageSrc: "../../assets/Piercing/17.png", 
+      imageAlt: "Photo 17"
+    },
+    {
+      imageSrc: "../../assets/Piercing/18.png", 
+      imageAlt: "Photo 18"
+    },
+    {
+      imageSrc: "../../assets/Piercing/19.png", 
+      imageAlt: "Photo 19"
+    },
+    {
+      imageSrc: "../../assets/Piercing/20.png", 
+      imageAlt: "Photo 20"
+    },
+    {
+      imageSrc: "../../assets/Piercing/21.png", 
+      imageAlt: "Photo 21"
+    },
+    {
+      imageSrc: "../../assets/Piercing/22.png", 
+      imageAlt: "Photo 22"
+    },
+    {
+      imageSrc: "../../assets/Piercing/23.png", 
+      imageAlt: "Photo 23"
+    },
+    {
+      imageSrc: "../../assets/Piercing/24.png", 
+      imageAlt: "Photo 24"
+    },
+    {
+      imageSrc: "../../assets/Piercing/25.png", 
+      imageAlt: "Photo 25"
+    },
+    {
+      imageSrc: "../../assets/Piercing/26.png", 
+      imageAlt: "Photo 26"
+    },
+    {
+      imageSrc: "../../assets/Piercing/27.png", 
+      imageAlt: "Photo 27"
+    },
+    {
+      imageSrc: "../../assets/Piercing/28.png", 
+      imageAlt: "Photo 28"
+    },
+    {
+      imageSrc: "../../assets/Piercing/24.png", 
+      imageAlt: "Photo 29"
+    },
+    {
+      imageSrc: "../../assets/Piercing/25.png", 
+      imageAlt: "Photo 30"
+    },
+    {
+      imageSrc: "../../assets/Piercing/26.png", 
+      imageAlt: "Photo 31"
+    },
+    {
+      imageSrc: "../../assets/Piercing/27.png", 
+      imageAlt: "Photo 32"
+    },
+    {
+      imageSrc: "../../assets/Piercing/28.png", 
+      imageAlt: "Photo 33"
+    },{
+      imageSrc: "../../assets/Piercing/24.png", 
+      imageAlt: "Photo 34"
+    },
+    {
+      imageSrc: "../../assets/Piercing/25.png", 
+      imageAlt: "Photo 35"
+    },
+    {
+      imageSrc: "../../assets/Piercing/26.png", 
+      imageAlt: "Photo 36"
+    },
+    {
+      imageSrc: "../../assets/Piercing/27.png", 
+      imageAlt: "Photo 37"
+    },
+    {
+      imageSrc: "../../assets/Piercing/28.png", 
+      imageAlt: "Photo 38"
+    },
+    {
+      imageSrc: "../../assets/Piercing/24.png", 
+      imageAlt: "Photo 39"
+    },
+    {
+      imageSrc: "../../assets/Piercing/25.png", 
+      imageAlt: "Photo 40"
+    },
+    {
+      imageSrc: "../../assets/Piercing/26.png", 
+      imageAlt: "Photo 41"
+    },
+    {
+      imageSrc: "../../assets/Piercing/27.png", 
+      imageAlt: "Photo 42"
+    },
+    {
+      imageSrc: "../../assets/Piercing/28.png", 
+      imageAlt: "Photo 43"
+    },
+    {
+      imageSrc: "../../assets/Piercing/24.png", 
+      imageAlt: "Photo 44"
+    },
+    {
+      imageSrc: "../../assets/Piercing/25.png", 
+      imageAlt: "Photo 45"
+    }
+  ];
 }
